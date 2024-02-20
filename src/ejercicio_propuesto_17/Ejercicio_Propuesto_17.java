46a2ed4e1145894632018237130b39f76d91234d
Santiago Marin Cardona <marincardona.santiago@hotmail.com>
2024-02-20 17:27:57 -0500

    Initial commit

---
+++ src/ejercicio_propuesto_17/Ejercicio_Propuesto_17.java
@@ -0,0 +1,15 @@
+
+package ejercicio_propuesto_17;
+
+public class Ejercicio_Propuesto_17 {
+
+    public static void main(String[] args) {
+        double R, Area, Loncir;
+        R = 15;
+        Area = Math.PI * Math.pow(R,2);
+        Loncir = Math.PI * (R*2);
+        System.out.println("El area del circulo es: " + Area);
+        System.out.println("La longitud de la circunferencia es: " + Loncir);
+    }
+    
+}
